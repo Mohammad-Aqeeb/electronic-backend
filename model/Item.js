d8ab3a0b32d5bd5249b785c07bdf298d99f95fdf
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-05-14 21:50:53 +0530

    adding item model

---
+++ model/Item.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const ItemSchema = new mongoose.Schema({ 
+    item_name: { 
+        type: String,
+        required: true 
+    },
+
+    item_category: {
+        type: String,
+        required: true 
+    },
+
+    item_price: {
+        type: Number,
+        required: true 
+    },
+
+    item_dsc: {
+        type: String, 
+        required: true
+    },
+
+    item_qty: {
+        type: Number,
+        required: true
+    },
+    item_image: {
+        type: String,
+        required: true
+    },
+    item_discount: {
+        type: String,
+        default: null 
+    }
+})
+
+module.exports = mongoose.model('Item', ItemSchema)
