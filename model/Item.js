577673bf716fdc8a2e7c3424707f1ed37356503f
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-05-16 21:22:22 +0530

    change in item model

--- model/Item.js
+++ model/Item.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
 
 const ItemSchema = new mongoose.Schema({ 
+    seller_id :{
+        type :mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required : true
+    },
     item_name: { 
         type: String,
         required: true 
