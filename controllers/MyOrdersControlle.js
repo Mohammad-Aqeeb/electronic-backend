f17c77ae4a7537bd067811bc4dcc36033e0502ee
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-05-17 22:02:21 +0530

    change in myOrder controller

--- controllers/MyOrdersControlle.js
+++ controllers/MyOrdersControlle.js
@@ -40,7 +40,9 @@ const MyOrdersController = {
       await sellerOrder.create({
         seller_id : item.seller_id,
         item_id,
+        user_id : user._id,
         user_name : user.name,
+        order_id : newOrder._id,
         item_category,
         item_name,
         item_price,
@@ -58,6 +60,7 @@ const MyOrdersController = {
       });
 
     } catch (error) {
+      console.log(error);
       return res.status(500).json({
         success: false,
         message: "Failed to place order",
@@ -115,6 +118,83 @@ const MyOrdersController = {
         message: "Failed to cancel order",
         error: error.message
       });
+    }
+  },
+
+  updateOrderStatus : async (req, res) => {
+
+    const id = req.params.id;
+    const { order_status } = req.body;
+
+    const allowedStatuses = [
+      "Pending",
+      "Confirmed",
+      "Packed",
+      "Shipped",
+      "Out for Delivery",
+      "Delivered",
+      "Cancelled"
+    ];
+
+    if(!allowedStatuses.includes(order_status)) {
+      return res.status(400).json({
+        success : false,
+        message: "Invalid status" 
+      });
+    }
+
+    console.log(id);
+
+    try {
+      const order = await MyOrder.findByIdAndUpdate(
+        id,
+        { order_status },
+        { new: true }
+      );
+
+      if (!order) {
+        return res.status(404).json({
+          success : false,
+          message : "Order not found" 
+        });
+      }
+
+      res.status(200).json({
+        success : true,
+        message : "Order status updated",
+        order 
+      });
+    } catch (error) {
+      res.status(500).json({ message: "Server error", error: error.message });
+    }
+  },
+
+  getOrderById : async (req,res) => {
+    const {id} = req.params;
+    try{
+      const data = await MyOrder.findById(id);
+
+      if(!data){
+        return res.status(500).json({
+          success : false,
+          message : "Order not found"
+        })
+      }
+
+      return res.status(200).json({
+        success : true,
+        message : "Order fetched",
+        data : data
+      })
+
+    }
+    catch(error){
+      console.log(error);
+      res.status(500).json({
+        success : false,
+        message : "server error",
+        error : error.message
+      })
     }
   }
 };
