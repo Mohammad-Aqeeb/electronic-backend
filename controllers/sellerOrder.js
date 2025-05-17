b8f7618eb04c7921c49b2de3ddf77bb16b422d52
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-05-17 22:02:07 +0530

    change in sellerOrder controller

--- controllers/sellerOrder.js
+++ controllers/sellerOrder.js
@@ -20,6 +20,54 @@ const sellerOrderController = {
                 error : error.message
             })
         }
+    },
+
+    updateOrderStatus : async (req, res) => {
+    
+        const id = req.params.id;
+        const { order_status } = req.body;
+    
+        const allowedStatuses = [
+          "Pending",
+          "Confirmed",
+          "Packed",
+          "Shipped",
+          "Out for Delivery",
+          "Delivered",
+          "Cancelled"
+        ];
+    
+        if(!allowedStatuses.includes(order_status)) {
+          return res.status(400).json({
+            success : false,
+            message: "Invalid status" 
+          });
+        }
+    
+    
+        try {
+          const order = await sellerOrder.findByIdAndUpdate(
+            id,
+            { order_status },
+            { new: true }
+          );
+    
+          if (!order) {
+            return res.status(404).json({
+              success : false,
+              message : "Order not found" 
+            });
+          }
+    
+          res.status(200).json({
+            success : true,
+            message : "Order status updated",
+            order 
+          });
+        } catch (error) {
+            console.log(error);
+          res.status(500).json({ message: "Server error", error: error.message });
+        }
     }
 
 }
