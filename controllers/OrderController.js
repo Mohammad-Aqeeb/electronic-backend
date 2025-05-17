edf6bc67bb38d293f9d0117404afa5cff1253747
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-05-17 22:09:12 +0530

    adding new controller

---
+++ controllers/OrderController.js
@@ -0,0 +1,75 @@
+const Order = require("../model/Order");
+
+const sellerOrderController = {
+    getSellerOrder : async (req, res)=>{
+        try{
+            const id = req.params.sellerId;
+            const data = await Order.find({seller_id : id});
+
+            res.status(200).json({
+                success : true,
+                message : "Order fetched success",
+                data : data
+            })
+        }
+        catch(error){
+            console.log(error);
+            res.status(500).json({
+                success : false,
+                message : "Internal server error",
+                error : error.message
+            })
+        }
+    },
+
+    updateOrderStatus : async (req, res) => {
+    
+        const id = req.params.id;
+        const { order_status } = req.body;
+    
+        const allowedStatuses = [
+          "Pending",
+          "Confirmed",
+          "Packed",
+          "Shipped",
+          "Out for Delivery",
+          "Delivered",
+          "Cancelled"
+        ];
+    
+        if(!allowedStatuses.includes(order_status)) {
+          return res.status(400).json({
+            success : false,
+            message: "Invalid status" 
+          });
+        }
+    
+    
+        try {
+          const order = await sellerOrder.findByIdAndUpdate(
+            id,
+            { order_status },
+            { new: true }
+          );
+    
+          if (!order) {
+            return res.status(404).json({
+              success : false,
+              message : "Order not found" 
+            });
+          }
+    
+          res.status(200).json({
+            success : true,
+            message : "Order status updated",
+            order 
+          });
+        } catch (error) {
+            console.log(error);
+          res.status(500).json({ message: "Server error", error: error.message });
+        }
+    }
+
+}
+
+module.exports = sellerOrderController;
