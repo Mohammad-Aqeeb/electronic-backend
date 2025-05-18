fa4f980bb62bfa698777d143fc4a4a384ba31507
Mohammad-Aqeeb <aqeeb2623@gmail.com>
2025-05-18 22:52:36 +0530

    change in Order model

--- model/Order.js
+++ model/Order.js
@@ -16,11 +16,38 @@ const OrderSchema = new mongoose.Schema({
     ref:'Item',
     required : true
   },
-  order_id : {
-    type : mongoose.Schema.Types.ObjectId,
-    ref : 'MyOrder',
-    required : true
+  item_name: {
+    type: String,
+    required: true,
   },
+  item_category: {
+    type: String,
+    required: true,
+  },
+  item_price: {
+    type: Number,
+    required: true,
+  },
+  item_dsc: {
+    type: String,
+    required: true,
+  },
+  item_qty: {
+    type: Number,
+    required: true,
+  },
+  item_image: {
+    type: String,
+    required: true,
+  },
+  item_discount: {
+    type: Number,
+    default: null,
+  },
+  item_subtotal: {
+    type: Number,
+    required: true,
+  }
 }, {
   timestamps: true
 });
